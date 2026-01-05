4e54feadb2205fc7695c0d9deb69a071331ca839
grcubes <gustavosamrod@gmail.om>
2026-01-05 00:24:48 -0800

    :feat: Improve code organization Specify image type in the name

--- clipboard-image.py
+++ clipboard-image.py
@@ -2,33 +2,48 @@ import os
 import glob
 from PIL import ImageGrab
 
-# Directory to save the clipboard image
-target_dir = os.path.expanduser(r"~\Pictures\ClipboardImage")
-os.makedirs(target_dir, exist_ok=True)
 
-# Delete old images
+def get_target_dir():
+    return os.path.expanduser(r"~\Pictures\ClipboardImage")
+
+
 def delete_previous_images():
-    for file in glob.glob(os.path.join(target_dir, "*")):
+    for file in glob.glob(os.path.join(get_target_dir(), "*")):
         try:
             os.remove(file)
         except Exception as e:
             print(f"Could not delete {file}: {e}")
 
-# Grab image from clipboard
-img = ImageGrab.grabclipboard()
 
-if img is None:
-    print("No image found in clipboard.")
-else:
-    print("\n")
-    delete_previous_images()
-    save_path = os.path.join(target_dir, "clipboard")
-
-    img.save(f"{save_path}.png", "PNG")
-    print(f"Saved clipboard image to {save_path}")
+def save_image(img):
+    if img is None:
+        print("No image found in clipboard.")
+    else:
+        print("\n")
     
-    try:
-        img.save(f"{save_path}.jpg", "JPEG")
-        print(f"Saved clipboard image to {save_path}")
-    except OSError:
-        print("OSError, skipping JPG...")
+        # Delete old images
+        delete_previous_images()
+    
+        save_image_path = os.path.join(get_target_dir(), "clipboard")
+
+        img.save(f"{save_image_path}PNG.png", "PNG")
+        
+        try:
+            img.save(f"{save_image_path}JPG.jpg", "JPEG")
+        except OSError:
+            print("OSError, skipping JPG...")
+        print(f"Saved clipboard image to {get_target_dir()}")
+
+
+def main():
+    # Create directory
+    os.makedirs(get_target_dir(), exist_ok=True)
+
+    # Grab image from clipboard
+    img = ImageGrab.grabclipboard()
+
+    save_image(img)
+
+
+if __name__ == "__main__":
+    main()
